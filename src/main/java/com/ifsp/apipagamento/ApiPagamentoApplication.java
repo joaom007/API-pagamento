9a0e483ce600f2c3d421d2a6470a4ad846486d2f
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-30 15:30:15 -0300

    Project API-pagamento created

---
+++ src/main/java/com/ifsp/apipagamento/ApiPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.apipagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsp/apipagamento/ApiPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.apipagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
